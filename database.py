915be0c3395490b438e8a2b498141d63010956f2
Ismael <ismael034@correo.ugr.es>
2023-11-01 10:51:57 +0100

    Separación de los componentes del programa

---
+++ database.py
@@ -0,0 +1,51 @@
+import pyodbc
+
+class database:
+    def __init__(self):
+        self.id_user = ""
+        self.password = ""
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            with open("connection.txt", "r") as f:
+                self.id_user = f.readline().strip()
+                self.password = f.readline().strip()
+
+                self.connection = pyodbc.connect(
+                    'Driver={Devart ODBC Driver for Oracle};'
+                    'Direct=True;'
+                    'Host=oracle0.ugr.es;'
+                    'Service Name=practbd.oracle0.ugr.es;'
+                    f'User ID={self.id_user};'
+                    f'Password={self.password};')
+
+                self.cursor = self.connection.cursor()
+
+        except pyodbc.Error as ex:
+            print("Error: ", ex)
+
+    def disconnect(self):
+        self.cursor.close()
+        self.connection.close()
+
+    def execute(self, query):
+        self.cursor.execute(query)
+
+    def fetchall(self):
+        return self.cursor.fetchall()
+
+    def fetchone(self):
+        return self.cursor.fetchone()
+
+    def commit(self):
+        self.connection.commit()
+
+    def rollback(self):
+        self.connection.rollback()
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
+        
--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-import pyodbc
+import database
 
 
-try:
-    with open("connection.txt", "r") as f:
-        id_user = f.readline().strip()
-        password = f.readline().strip()
-        database = f.readline().strip()
+def main():
+    db = database.database()
+    db.connect()
 
-        connection = pyodbc.connect(
-            'Driver={Devart ODBC Driver for Oracle};'
-            'Direct=True;'
-            'Host=oracle0.ugr.es;'
-            'Service Name=practbd.oracle0.ugr.es;'
-            f'User ID={id_user};'
-            f'Password={password};')
+    db.execute("SELECT * FROM PRUEBA1")
+    for row in db.fetchall():
+        print(row)
+    
 
-        cursor = connection.cursor()
-
-        cursor.execute("SELECT * FROM mis_sesiones")
-
-        for row in cursor:
-            print(row)
-
-        cursor.close()
-        connection.close()
-
-
-except pyodbc.Error as ex:
-    print("Error:", ex)
+if __name__ == "__main__":
+    main()
