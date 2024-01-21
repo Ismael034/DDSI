fcb77255c3e5bc52276dd244663f4a262e14db13
Ismael <ismael034@correo.ugr.es>
2024-01-21 12:20:43 +0100

    delete triger

--- app/query/query.py
+++ app/query/query.py
@@ -46,6 +46,7 @@ class query:
             self.db.execute("DROP TABLE IF EXISTS Usuario")
             
             self.db.execute(f"DROP TRIGGER IF EXISTS actualizar_saldo")
+            self.db.execute(f"DROP TRIGGER IF EXISTS crear_perfil")
             self.db.commit()
         except Exception as ex:
             logging.error("Error deleting tables: ", ex)
